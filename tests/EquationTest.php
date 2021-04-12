e7a6792b308153cbc6495bd837fba172b60f5ad8
LooNny228 <70636567+LooNny228@users.noreply.github.com>
2021-04-12 09:18:48 +0300

    Update EquationTest.php

--- tests/EquationTest.php
+++ tests/EquationTest.php
@@ -2,23 +2,23 @@
 
 use PHPUnit\Framework\TestCase;
 use Gaivoronskiy\Exception;
-use Gaivoronskiy\Line;
+use Gaivoronskiy\Equation;
 
 class EquationTest extends TestCase
 {
     public function testEquation()
     {
         $equation = new Equation();
-        $this->assertEquals([-12], $equation->equation(6, 72));
-        $this->assertEquals([1], $line->line(-1, 1));
+        $this->assertEquals([-12], $equation->li_solve(6, 72));
+        $this->assertEquals([1], $equation->li_solve(-1, 1));
     }
 
     public function testEquationBad()
     {
         $line = new Line();
-        $this->expectExceptionMessage('This line equation does not exist!');
+        $this->expectExceptionMessage('Ошибка: уравнения не существует.');
         $this->expectException(Exception::class);
-        $line->line(0, -7);
-        $line->line(0, 2);
+        $equation->li_solve(0, -100);
+        $equation->li_solve(0, 46);
     }
 }
